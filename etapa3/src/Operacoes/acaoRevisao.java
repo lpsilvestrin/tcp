c1cf9c7ae139d4f68943c4d7d21606cf72370d86
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-12-04 13:13:01 -0200

    execute

--- etapa3/src/Operacoes/acaoRevisao.java
+++ etapa3/src/Operacoes/acaoRevisao.java
@@ -1,11 +1,41 @@
 package Operacoes;
 
+import java.util.ArrayList;
+
+import Interface.UIUtils;
+
 public class acaoRevisao {
 
 	private Operacoes operacoes;
 
+	public acaoRevisao(Operacoes operacoes) {
+		this.operacoes = operacoes;
+	}
+	
 	public void executar() {
-
+		
+		UIUtils uiUtils = UIUtils.INSTANCE;
+		
+		ArrayList<String> conferencias = this.operacoes.getListaConferenciasSiglas();
+		for (String sigla : conferencias) {
+			System.out.println(sigla);
+		}
+		
+		String siglaSelecionada = uiUtils.readString("Conferência");
+		ArrayList<String> artigos = this.operacoes.getListaArtigosString(siglaSelecionada);
+		for (String artigoString : artigos) {
+			System.out.println(artigoString);
+		}
+		
+		int idArtigo = uiUtils.readInteger("Id do artigo");
+		ArrayList<String> revisores = this.operacoes.getListaRevisoresString(idArtigo);
+		for (String revString : revisores) {
+			System.out.println(revString);
+		}
+		
+		int idRevisor = uiUtils.readInteger("Id do revisor");
+		int nota = uiUtils.readInteger("Nota");
+		this.operacoes.addRevisaoArtigo(idArtigo, idRevisor, nota);
 	}
 
 }
