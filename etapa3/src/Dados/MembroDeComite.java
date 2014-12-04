cd133fbd19773dfc6ec14744061181ab630712be
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-12-03 23:40:20 -0200

    Revior virou filho do membrodecomite e tem os atributos referentes a revisao

--- etapa3/src/Dados/MembroDeComite.java
+++ etapa3/src/Dados/MembroDeComite.java
@@ -2,19 +2,9 @@ package Dados;
 
 public class MembroDeComite extends Pesquisador {
 
-	private int qtdArtigosAlocados;
-
 	public MembroDeComite(int id, String nome, Afiliacao afiliacao) {
 		super(id, nome, afiliacao);
-		this.qtdArtigosAlocados = 0;
 	}
 
-	public void addQtdArtigosAlocados() {
-		this.qtdArtigosAlocados += 1;
-	}
-
-	public int getQtdArtigosAlocados() {
-		return this.qtdArtigosAlocados;
-	}
 
 }
--- etapa3/src/Dados/Revisor.java
+++ etapa3/src/Dados/Revisor.java
@@ -1,9 +1,20 @@
 package Dados;
 
-public class Revisor extends Pesquisador {
+public class Revisor extends MembroDeComite {
 
+	private int qtdArtigosAlocados;
+	
 	public Revisor(int id, String nome, Afiliacao afiliacao) {
 		super(id, nome, afiliacao);
+		this.qtdArtigosAlocados = 0;
 	}
 
+	public void addQtdArtigosAlocados() {
+		this.qtdArtigosAlocados += 1;
+	}
+
+	public int getQtdArtigosAlocados() {
+		return this.qtdArtigosAlocados;
+	}
+	
 }
