20749df20988bf768930a9ab5ff90dd23d30dd02
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-12-04 14:54:41 -0200

    repassando qtdArtigosAlocados para membroDeComite

--- etapa3/src/Dados/Revisor.java
+++ etapa3/src/Dados/Revisor.java
@@ -2,19 +2,8 @@ package Dados;
 
 public class Revisor extends MembroDeComite {
 
-	private int qtdArtigosAlocados;
-
 	public Revisor(MembroDeComite membroDeComite) {
 		super(membroDeComite.id, membroDeComite.nome, membroDeComite.afiliacao);
-		this.qtdArtigosAlocados = 0;
-	}
-	
-	public void addQtdArtigosAlocados() {
-		this.qtdArtigosAlocados += 1;
-	}
-
-	public int getQtdArtigosAlocados() {
-		return this.qtdArtigosAlocados;
 	}
 	
 }
