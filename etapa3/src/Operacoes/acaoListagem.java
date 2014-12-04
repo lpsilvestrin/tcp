b72dcf56929200418a4bfb13fcf1e414174d4fd3
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-12-04 15:32:50 -0200

    em andamento

--- etapa3/src/Operacoes/acaoListagem.java
+++ etapa3/src/Operacoes/acaoListagem.java
@@ -1,11 +1,32 @@
 package Operacoes;
 
+import java.util.ArrayList;
+
+import Interface.UIUtils;
+
 public class acaoListagem {
 
 	private Operacoes operacoes;
 
+	public acaoListagem(Operacoes operacoes) {
+		this.operacoes = operacoes;
+	}
+	
 	public void executar() {
-
+		UIUtils uiUtils = UIUtils.INSTANCE;
+		
+		ArrayList<String> conferencias = this.operacoes.getListaConferenciasSiglas();
+		for (String sigla : conferencias) {
+			System.out.println(sigla);
+		}
+		
+		String siglaConf = uiUtils.readString("Conferência");
+		boolean pendentes = this.operacoes.verificaRevisoesPendentes(siglaConf);
+		if(pendentes) {
+			System.out.println("Essa conferência possui revisões pendentes");
+		} else {
+			
+		}
 	}
 
 }
