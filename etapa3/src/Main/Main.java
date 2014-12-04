8f32291f1ec442434aa2fe2d953f91384c815643
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-12-04 17:39:05 -0200

    main

---
+++ etapa3/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Interface.InterfacePrincipal;
+import Operacoes.Operacoes;
+
+public class Main {
+	
+	public static void main(String [] args) {
+		Operacoes operacoes = new Operacoes();
+		InterfacePrincipal iu = new InterfacePrincipal(operacoes);
+		
+		iu.iniciaInterface();
+	}
+	
+}
