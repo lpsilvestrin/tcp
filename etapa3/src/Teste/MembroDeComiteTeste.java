d14b0b321eae88e1ed6199651128831d16cad728
Aleuck <Aleuck@github.com>
2014-12-05 11:54:16 -0200

    Testes MembroDeComite

--- etapa3/src/Teste/MembroDeComiteTeste.java
+++ etapa3/src/Teste/MembroDeComiteTeste.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import Dados.*;
 
 public class MembroDeComiteTeste {
+	private Afiliacao afiliacao1;
+	private Pesquisador pesquisador1;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -14,21 +17,34 @@ public class MembroDeComiteTeste {
 
 	@Before
 	public void setUp() throws Exception {
+		afiliacao1 = new Afiliacao("afiliacao");
+		pesquisador1 = new Pesquisador(0, "teste", afiliacao1);
 	}
 
 	@Test
 	public void testMembroDeComite() {
-		fail("Not yet implemented");
+		MembroDeComite m1 = new MembroDeComite(0, "teste", afiliacao1);
+		MembroDeComite m2 = new MembroDeComite(pesquisador1);
+		assertNotNull(m1);
+		assertNotNull(m2);
 	}
 
 	@Test
 	public void testAddQtdArtigosAlocados() {
-		fail("Not yet implemented");
+		MembroDeComite m1 = new MembroDeComite(0, "teste", afiliacao1);
+		m1.addQtdArtigosAlocados();
+		m1.addQtdArtigosAlocados();
+		m1.addQtdArtigosAlocados();
 	}
 
 	@Test
 	public void testGetQtdArtigosAlocados() {
-		fail("Not yet implemented");
+		MembroDeComite m1 = new MembroDeComite(0, "teste", afiliacao1);
+		assertEquals(0, m1.getQtdArtigosAlocados());
+		m1.addQtdArtigosAlocados();
+		assertEquals(1, m1.getQtdArtigosAlocados());
+		m1.addQtdArtigosAlocados();
+		assertEquals(2, m1.getQtdArtigosAlocados());
 	}
 
 }
